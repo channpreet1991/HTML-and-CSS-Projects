ca12ddd14ceaf646ba7584f973afad2105eeec0e
channpreet1991 <channpreet.learning@gmail.com>
2021-01-03 19:23:55 -0400

    2nd commit

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+alert("Alert");
+function My_First_Function() {
+  var String = "Kiss me, I'm Irish!";
+  var result = String.fontcolor("green");
+  document.getElementById("irish").innerHTML = result;
+}
